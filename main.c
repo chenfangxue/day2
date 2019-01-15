ece4344e4614219bf6c95cb10acee1e90a206836
cfx <you@example.com>
2019-01-15 19:01:49 +0800

    第二次更改

--- main.c
+++ main.c
@@ -5,28 +5,51 @@ int main()
 
     printf("进入存钱取钱管理系统\n");
 
-    return 0;
+    
     int qian = 0;
-    int code;
+    int code = 0;
     int cun;
     int jine;
+    int qu;
+    while(2){
+        printf("1--存钱\n");
+        printf("2--取钱\n");
+        printf("3--余额查询\n");
+        printf("4--退出系统\n");
+        printf("请选择所需向项目符号\n");
+        scanf("%d",&code);
+    
     if (code == 1)
     {
-        printf("请输入要存入的金额\n"); 
-        scanf("%d",&cun);
-        qian+=jine;
+        char x;
+        printf("请输入要存入的金额");
+        scanf("%d", &cun);
+        qian += jine;
         printf("存入成功，点击回车继续\n");
-        scanf("%c",&x);
-        scanf("%c",&x);
+        scanf("%c", &x);
+        scanf("%c", &x);
     }
     if (code == 2)
     {
-        printf("请输入要取出的金额\n");
+        char x;
+        printf("请输入要取出的金额");
+        scanf("%d", &qu);
+        if (qian <= qian)
+            printf("您输入的金额有误");
+        else
+            qian = qian - jine;
+        printf("取钱成功，点击回车继续\n");
+        scanf("%c", &x);
+        scanf("%c", &x);
     }
     if (code == 3)
     {
-    }
+        printf("");
+   }
     if (code == 4)
     {
     }
+    }
+    return 0;
+    
 }
