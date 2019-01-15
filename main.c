04387500595d56946872411cce55d850a83ffa92
cfx <you@example.com>
2019-01-15 11:25:43 +0800

    搭建基本结构框架

--- main.c
+++ main.c
@@ -2,8 +2,22 @@
 
 int main(){
 
-    printf("hello world!");
+    printf("进入存钱取钱管理系统\n");
 
     return 0;
+    int code;
+    if(code == 1){
+
+    }
+    if(code == 2){
+        
+    }
+    if(code == 3){
+        
+    }
+    if(code == 4){
+        
+    }
+
 
 }
